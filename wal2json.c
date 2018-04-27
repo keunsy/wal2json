8df48ff06645acb22879e32b602450e16c5aebf2
陈榕 <chenr@fxiaoke.com>
2018-04-27 18:22:02 +0800

    fix

--- wal2json.c
+++ wal2json.c
@@ -1065,7 +1065,7 @@ pg_decode_change(LogicalDecodingContext *ctx, ReorderBufferTXN *txn,
         pfree(lsn_str);
     }
 
-    if(use_socket){
+    if(data->use_socket){
         appendStringInfo(ctx->out, "\"topic\":\"%s\",", data->topic);
     }
     appendStringInfo(ctx->out, "\"slot_name\":\"%s\",", NameStr(ctx->slot->data.name));
