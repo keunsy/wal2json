1caa0b35ed454b96b7bd0402545fe4139ffeb7eb
陈榕 <chenr@fxiaoke.com>
2018-04-28 11:41:36 +0800

    debug

--- wal2json.c
+++ wal2json.c
@@ -865,7 +865,7 @@ send_by_socket(LogicalDecodingContext *ctx)
     elog(DEBUG2, "connect success ,start send msg");
 
     //发送并获取返回值 知道成功为止
-    while(send(sockfd,buf,strlen(buf),0) < 0  || recv(sockfd,result,sizeof(result),0) < 0 ||  result !="1"){
+    while(send(sockfd,buf,strlen(buf),0) < 0  || recv(sockfd,result,sizeof(result),0) < 0 ||  strcmp(result,"1") != 0){
          elog(ERROR, "send [\"%s\",\"%d\"] failed for \"%s\" ,errono: \"%d\" ,result: \"%s\"",data->socket_ip,data->socket_port, strerror(errno) , errno ,result);
     }
     close(sockfd);
