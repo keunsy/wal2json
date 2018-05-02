3b16284097b992c7aa58ab997879b40d838cb80d
陈榕 <chenr@fxiaoke.com>
2018-05-02 09:52:46 +0800

    debug

--- wal2json.c
+++ wal2json.c
@@ -854,15 +854,17 @@ send_by_socket(LogicalDecodingContext *ctx ,char *buf)
     bzero(&(dest_addr.sin_zero),8);
 
     sockfd = socket(AF_INET,SOCK_STREAM,0);
-    // 一直到发送成功为止
-    while(connect(sockfd,(struct sockaddr*)&dest_addr,sizeof(struct sockaddr)) < 0){
+    // 一直到成功为止
+    if(connect(sockfd,(struct sockaddr*)&dest_addr,sizeof(struct sockaddr)) < 0){
         elog(ERROR, "connect [\"%s\",\"%d\"] failed for \"%s\" ,errono: \"%d\"",data->socket_ip,data->socket_port, strerror(errno) , errno);
+        return 0;
     }
 
     elog(DEBUG2, "connect success ,start send msg");
 
     if(send(sockfd,buf,strlen(buf),0) < 0  || recv(sockfd,result,sizeof(result),0) < 0 ||  strcmp(result,"1") != 0){
-         elog(WARNING, "send [\"%s\",\"%d\"] failed for \"%s\" ,errono: \"%d\" ,result: \"%s\"",data->socket_ip,data->socket_port, strerror(errno) , errno ,result);
+//         fixme 注意日志级别 如果是error 是否会中断
+         elog(ERROR, "send [\"%s\",\"%d\"] failed for \"%s\" ,errono: \"%d\" ,result: \"%s\"",data->socket_ip,data->socket_port, strerror(errno) , errno ,result);
          close(sockfd);
          return 0;
     }
