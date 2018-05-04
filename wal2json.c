78784810cc678456b7090f71252468bd424166cf
陈榕 <chenr@fxiaoke.com>
2018-05-04 14:44:16 +0800

    debug

--- wal2json.c
+++ wal2json.c
@@ -742,41 +742,40 @@ send_by_socket(LogicalDecodingContext *ctx ,char *buf)
 
     sockfd = socket(AF_INET,SOCK_STREAM,0);
 
-
     //超时设置
-//    int error=-1, len;
-//    len = sizeof(int);
-//    struct timeval tm;
-//    fd_set set;
-//    unsigned long ul = 1;
-//
-//    ioctl(sockfd, FIONBIO, &ul); //设置为非阻塞模式
-//
-//    bool ret = false;
-//    if(connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr)) < 0 ){
-//        tm.tv_sec = 10;
-//        tm.tv_usec = 0;
-//        FD_ZERO(&set);
-//        FD_SET(sockfd, &set);
-//        if( select(sockfd+1, NULL, &set, NULL, &tm) > 0) {
-//            getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, (socklen_t *)&len);
-//            if(error == 0) ret = true;
-//            else ret = false;
-//        } else ret = false;
-//    }else ret = true;
-//
-//    ul = 0;
-//    ioctl(sockfd, FIONBIO, &ul); //设置为阻塞模式
-//    if(!ret){
-//        elog(WARNING, "connect [%s,%d] failed for \"%s\" ,errono: \"%d\"",data->socket_ip,data->socket_port, strerror(errno) , errno);
-//        close(sockfd);
-//        return 0;
-//    }
+    int error=-1, len;
+    len = sizeof(int);
+    struct timeval tm;
+    fd_set set;
+    unsigned long ul = 1;
 
-    if(connect(sockfd,(struct sockaddr*)&dest_addr,sizeof(struct sockaddr)) < 0){
-        elog(WARNING, "connect [\"%s\",%d] failed for \"%s\" ,errono: \"%d\"",data->socket_ip,data->socket_port, strerror(errno) , errno);
+    ioctl(sockfd, FIONBIO, &ul); //设置为非阻塞模式
+
+    bool ret = false;
+    if(connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr)) < 0 ){
+        tm.tv_sec = 10;
+        tm.tv_usec = 0;
+        FD_ZERO(&set);
+        FD_SET(sockfd, &set);
+        if( select(sockfd+1, NULL, &set, NULL, &tm) > 0) {
+            getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, (socklen_t *)&len);
+            if(error == 0) ret = true;
+            else ret = false;
+        } else ret = false;
+    }else ret = true;
+
+    ul = 0;
+    ioctl(sockfd, FIONBIO, &ul); //设置为阻塞模式
+    if(!ret){
+        elog(WARNING, "connect [%s,%d] failed for \"%s\" ,errono: \"%d\"",data->socket_ip,data->socket_port, strerror(error) , error);
+        close(sockfd);
         return 0;
     }
+
+//    if(connect(sockfd,(struct sockaddr*)&dest_addr,sizeof(struct sockaddr)) < 0){
+//        elog(WARNING, "connect [\"%s\",%d] failed for \"%s\" ,errono: \"%d\"",data->socket_ip,data->socket_port, strerror(errno) , errno);
+//        return 0;
+//    }
 
     elog(DEBUG2, "connect success ,start send msg");
 
@@ -1076,7 +1075,6 @@ pg_decode_change(LogicalDecodingContext *ctx, ReorderBufferTXN *txn,
 
     //myupdate 转入socket 并将ctx->初始化 为事务数量
     if (data->socket_port != 0 && data->socket_ip !=NULL){
-
 
         if(data->nr_changes % data->batch_size == 0 || data->nr_changes >= txn->nentries ){
             //拼接
