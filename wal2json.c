083322491c92cb60db7b4cf80e52d202d87efaad
陈榕 <chenr@fxiaoke.com>
2018-05-07 18:38:56 +0800

    debug

--- wal2json.c
+++ wal2json.c
@@ -742,7 +742,7 @@ send_by_socket(LogicalDecodingContext *ctx, char *buf) {
     close(sockfd);
     initStringInfo(ctx->out);
     //回收防止内存泄露
-    pfree(buf);
+//    pfree(buf);
 
     return 0;
 }
